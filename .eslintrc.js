96f36507fd76f28578219d3307a6da78e0f27a36
Tom Glenn <tomglenn91@gmail.com>
2022-03-13 13:48:51 +0000

    init commit with problem

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'react-app',
+		'plugin:import/typescript'
+	],
+	settings: {
+		'import/resolver': {
+			node: {
+				paths: ['src'],
+			},
+			typescript: {
+				project: [path.join(__dirname, 'tsconfig.json')],
+			},
+		},
+	},
+	rules: {
+		'import/no-unused-modules': [
+			'warn',
+			{
+				unusedExports: true,
+				missingExports: true,
+				src: [path.join(__dirname, 'src')],
+			},
+		],
+	}
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test'
+import TestTwo from 'components/TestTwo'
 
 function App() {
   return (
